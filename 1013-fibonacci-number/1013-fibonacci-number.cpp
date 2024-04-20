226265bd9c2b43cb3e2f4d9c87335b7b01941815
AjayBhaskar Athi <114720857+Ajaybhaskar-Athi@users.noreply.github.com>
2024-04-21 00:18:33 +0530

    Time: 0 ms (100%), Space: 7.2 MB (34.57%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fib(int n) {
+        int prev=1;
+        int prev2=0;
+        if(n<=1)return n;
+        for(int i=2;i<=n;i++){
+            int curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
+        }
+      return prev;
+    }
+};
